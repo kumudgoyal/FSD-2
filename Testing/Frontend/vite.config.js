ad6b8df3a5112ec66afea991f2bdaa37f6b7e04d
Kumud <Kumudgoyal@gmail.com>
2026-04-22 15:48:45 +0530

    Docker

--- Testing/Frontend/vite.config.js
+++ Testing/Frontend/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: '0.0.0.0',
+    port: 5173, // dev server (optional)
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173, // build preview (important for Docker)
+  },
   test: {
     environment: "jsdom",
     globals: true,
